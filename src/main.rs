2e681f0096a42eac327819466e4b8bae9cd3488f
chrisdfe <christopher.d.ferris@gmail.com>
2024-09-01 22:54:45 -0700

    Button click handlers

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ fn random_color() -> Color {
 
 struct UITheme {
   background_primary: Color,
-  interactive_background_primary: Color,
+  interactive_background_primary_default: Color,
+  interactive_background_primary_hovered: Color,
   foreground_primary: Color,
 }
 
@@ -19,13 +20,28 @@ impl UITheme {
   fn random() -> Self {
     Self {
       background_primary: random_color(),
-      interactive_background_primary: random_color(),
+      interactive_background_primary_default: random_color(),
+      interactive_background_primary_hovered: random_color(),
       foreground_primary: random_color(),
     }
   }
+
+  pub fn randomize(&mut self) {
+    let r = Self::random();
+    self.background_primary = r.background_primary;
+    self.interactive_background_primary_default = r.interactive_background_primary_default;
+    self.interactive_background_primary_hovered = r.interactive_background_primary_hovered;
+    self.foreground_primary = r.foreground_primary;
+  }
 }
 
-type ButtonRenderFn = fn(btn: &Button, theme: &UITheme) -> ();
+type ButtonRenderFn = fn(btn: &Button, is_hovered: bool, theme: &UITheme) -> ();
+
+struct ButtonClickHandlerContext<'a> {
+  ui_theme: &'a mut UITheme,
+}
+
+type ButtonClickHandler = fn(button: &Button, ctx: ButtonClickHandlerContext);
 
 struct Button {
   pub x: f32,
@@ -34,20 +50,21 @@ struct Button {
   pub height: f32,
   pub render_background: ButtonRenderFn,
   pub render_foreground: ButtonRenderFn,
+  pub on_click: Option<ButtonClickHandler>,
 }
 
-fn render_rectangular_button(button: &Button, ui_theme: &UITheme) {
+fn render_rectangular_button(button: &Button, is_hovered: bool, ui_theme: &UITheme) {
+  let color = if is_hovered {
+    ui_theme.interactive_background_primary_hovered
+  } else {
+    ui_theme.interactive_background_primary_default
+  };
+
   //
-  draw_rectangle(
-    button.x,
-    button.y,
-    button.width,
-    button.height,
-    ui_theme.interactive_background_primary,
-  );
+  draw_rectangle(button.x, button.y, button.width, button.height, color);
 }
 
-fn render_close_button_foreground(button: &Button, ui_theme: &UITheme) {
+fn render_close_button_foreground(button: &Button, _: bool, ui_theme: &UITheme) {
   const PADDING: f32 = 10.;
 
   let width = button.width - (PADDING * 2.);
@@ -61,12 +78,20 @@ fn render_close_button_foreground(button: &Button, ui_theme: &UITheme) {
   draw_circle(x, y, radius, ui_theme.foreground_primary);
 }
 
+fn randomize_ui_theme_click_handler(_: &Button, ctx: ButtonClickHandlerContext) {
+  // noop
+  let ButtonClickHandlerContext { ui_theme } = ctx;
+  ui_theme.randomize();
+}
+
 #[macroquad::main("DrawingPad")]
 async fn main() {
   // Seed rng
   rand::srand(macroquad::miniquad::date::now() as _);
 
   let mut ui_theme = UITheme::random();
+  let mut hovered_button_idx: i32 = -1;
+  let mut clicked_button_idx: i32 = -1;
   let buttons = vec![Button {
     x: 100.,
     y: 100.,
@@ -74,6 +99,7 @@ async fn main() {
     height: 100.,
     render_background: render_rectangular_button,
     render_foreground: render_close_button_foreground,
+    on_click: Some(randomize_ui_theme_click_handler),
   }];
 
   loop {
@@ -82,13 +108,56 @@ async fn main() {
     }
 
     // mouseover/mouseout
+    {
+      let (mouse_x, mouse_y) = mouse_position();
+      let mut has_updated_hover: bool = false;
+      for (idx, button) in buttons.iter().enumerate() {
+        if mouse_x > button.x
+          && mouse_x < button.x + button.width
+          && mouse_y > button.y
+          && mouse_y < button.y + button.height
+        {
+          hovered_button_idx = idx as i32;
+          has_updated_hover = true;
+          break;
+        }
+      }
+
+      if has_updated_hover {
+        if is_mouse_button_pressed(MouseButton::Left) {
+          clicked_button_idx = hovered_button_idx;
+        } else {
+          clicked_button_idx = -1;
+        }
+      } else {
+        hovered_button_idx = -1;
+        clicked_button_idx = -1;
+      }
+    }
+
+    // run button event handlers
+    {
+      if clicked_button_idx >= 0 {
+        let clicked_button = &buttons[clicked_button_idx as usize];
+
+        if let Some(on_click) = clicked_button.on_click {
+          (on_click)(
+            &clicked_button,
+            ButtonClickHandlerContext {
+              ui_theme: &mut ui_theme,
+            },
+          )
+        }
+      }
+    }
 
     // Render
     clear_background(ui_theme.background_primary);
 
-    for button in buttons.iter() {
-      (button.render_background)(button, &ui_theme);
-      (button.render_foreground)(button, &ui_theme);
+    for (idx, button) in buttons.iter().enumerate() {
+      let is_hovered = idx as i32 == hovered_button_idx;
+      (button.render_background)(button, is_hovered, &ui_theme);
+      (button.render_foreground)(button, is_hovered, &ui_theme);
     }
 
     next_frame().await
